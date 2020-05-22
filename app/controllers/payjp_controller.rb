697bf47c1bf12ae9c10496e40afd8109d63502d7
kokkedx <55970331+kokkedx@users.noreply.github.com>
2020-05-22 16:10:23 +0900

    Update payjp_controller.rb

--- app/controllers/payjp_controller.rb
+++ app/controllers/payjp_controller.rb
@@ -1,23 +1,3 @@
 class PayjpController < ApplicationController
 
-  def index
-  end
-
-
-  def create
-    pay_item
-  end
-
-
-  private
-
-  def pay_item
-    Payjp.api_key = 'sk_test_a309a0a09c01fc5695e76319'
-    charge = Payjp::Charge.create(
-      amount: 5555,
-      card: params[:token],
-      currency:'jpy'
-    )
-  end
-
-end
+  
